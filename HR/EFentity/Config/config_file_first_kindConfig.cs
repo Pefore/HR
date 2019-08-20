bee9330c7ac5b8ea8a2b22a9b30982422dadcd18
LiPanDN <1583984259@qq.com>
2019-08-20 11:22:14 +0800

    实体类一级二级表

---
+++ HR/EFentity/Config/config_file_first_kindConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration;
+
+namespace EFentity.Config
+{
+    public class config_file_first_kindConfig:EntityTypeConfiguration<config_file_first_kind>
+    {
+        /// <summary>
+        /// 一级机构设置   
+        /// </summary>
+        public config_file_first_kindConfig()
+        {
+            this.ToTable(nameof(config_file_first_kind));
+            this.Property(e => e.first_kind_name).HasMaxLength(60);
+        }
+    }
+}
---
+++ HR/EFentity/Config/config_file_second_kindConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration;
+
+namespace EFentity.Config
+{
+    public class config_file_second_kindConfig:EntityTypeConfiguration<config_file_second_kind>
+    {
+        /// <summary>
+        /// 二级机构设置       
+        /// </summary>
+        public config_file_second_kindConfig()
+        {
+            this.ToTable(nameof(config_file_second_kind));
+            this.Property(e => e.first_kind_name).HasMaxLength(60);
+            this.Property(e => e.second_kind_name).HasMaxLength(60);
+        }
+    }
+}
