b41c7033f2a8c715c7828997c6c622fa4af36775
asus <asus@>
2019-08-20 08:44:00 +0800

    登录

---
+++ HR/EFentity/Config/UserConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFentity.Config
+{
+   public class UserConfig: EntityTypeConfiguration<user>
+    {
+        public UserConfig()
+        {
+            this.ToTable(nameof(user));
+            this.Property(e => e.user_name).HasMaxLength(60).IsRequired();
+            this.Property(e => e.user_true_name).HasMaxLength(60).IsRequired();
+            this.Property(e => e.user_password).HasMaxLength(60).IsRequired();
+        }
+    }
+}
