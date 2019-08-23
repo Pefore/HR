3d8ae496e880c767d7a3160d08164162c1358e52
Pefore <xuwenbin...2000>
2019-08-23 20:13:12 +0800

    123123

--- HR/EFentity/MyDbcontext.cs
+++ HR/EFentity/MyDbcontext.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
 using System.Reflection;
+using EFentity.Config;
 
 namespace EFentity
 {
@@ -20,11 +21,13 @@ namespace EFentity
             base.OnModelCreating(modelBuilder);
             modelBuilder.Configurations.AddFromAssembly(Assembly.GetExecutingAssembly());
         }
-        public DbSet<user> user { set; get; }
         public DbSet<config_public_char> config_public_char { get; set; }
         public DbSet<config_major_kind> config_major_kind { set; get; }
         public DbSet<config_major> config_major { set; get; }
         public DbSet<Position_Name_Set> Position_Name_Set { set; get; }
         public DbSet<users> user { set; get; }
+        public DbSet<config_file_first_kind> config_file_first_kind{ set; get; }
+        public DbSet<config_file_second_kind> config_file_second_kind { set; get; }
+        public DbSet<users> users { set; get; }
     }
 }
